aedb3fd17787e033fd5c3bdaea13cc8892dc1b57
Valentinas Bakaitis <v.bakaitis@gmail.com>
2014-01-17 16:33:31 +1300

    Add multithreading, so the server could serve on two sockets at once. If the remote ip is not provided, it will only serve on localhost

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-import BaseHTTPServer, tpb, sys, ConfigParser
+import BaseHTTPServer, tpb, sys, ConfigParser, thread
 from urlparse import urlparse, parse_qs
 from utorrent import Utorrent as ut
 
 config = ConfigParser.ConfigParser()
 config.read("server.config")
-ip = config.get("params","ip")
 port = int(config.get("params","port"))
 
 class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def do_GET(self):
+        ip = self.server.socket.getsockname()[0]
         self.send_response(200)
         self.send_header("content-type", "text/html")
         self.end_headers()
@@ -32,13 +32,24 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.wfile.write(result.encode('UTF-8'))
 
 HandlerClass = RequestHandler 
-ServerClass  = BaseHTTPServer.HTTPServer
-
-server_address = (ip, port)
-
 HandlerClass.protocol_version = "HTTP/1.0" 
 
-httpd = ServerClass(server_address, HandlerClass)
-sa = httpd.socket.getsockname()
-print "Serving HTTP on", sa[0], "port", sa[1], "..."
-httpd.serve_forever()
+ServerClass  = BaseHTTPServer.HTTPServer
+
+server_local_address = ('127.0.0.1', port)
+httpd_local = ServerClass(server_local_address, HandlerClass)
+local_socket = httpd_local.socket.getsockname()
+thread.start_new_thread(httpd_local.serve_forever,())
+print "Thread 1 is Serving HTTP on", local_socket[0], "port", local_socket[1], "..."
+
+try:
+    ip = config.get("params","ip")
+    server_remote_address = (ip, port)
+    httpd_remote = ServerClass(server_remote_address, HandlerClass)
+    remote_socket = httpd_remote.socket.getsockname()
+    thread.start_new_thread(httpd_remote.serve_forever, ())
+    print "Thread 2 is Serving HTTP on", remote_socket[0], "port", remote_socket[1], "..."
+except ConfigParser.NoOptionError:
+    print "no ip was provided in server.config, so serving only on localhost"
+while 1:
+    pass
